cb2eea981c101151ced4d9c8ca5a755c2481e857
Vinayak-fr <Vinayak.patil@acelucid.com>
2025-12-27 13:26:11 +0530

    simple connection

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("Project Setup")
+import { WebSocketServer, WebSocket } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+let userCount = 0;
+let allSockets: WebSocket[] = [];
+
+wss.on("connection", (socket) => {
+  allSockets.push(socket);
+  userCount += 1;
+  console.log("User connected #" + userCount);
+
+  socket.on("message", (message) => {
+    for (let i = 0; i < allSockets.length; i++) {
+      const s = allSockets[i];
+      if (s) {
+        s.send(message.toString() + ": sent from the server");
+      }
+    }
+  });
+});
